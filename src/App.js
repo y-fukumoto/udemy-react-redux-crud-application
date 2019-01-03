9e977ef9e0e6281e2106aca3cac8adcf57c9e9e6
y fu <helmutlang1028@gmail.com>
2019-01-03 21:08:06 +0900

    before redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+const App = () => {
+  return (
+    <Counter />
+  )
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1} )
+  }
+
+  handleMinusButton = () => {
+    this.setState({count: this.state.count -1})
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log('click')}} name="bar" />
-      </React.Fragment>
+      <div>
+        count: {this.state.count}
+        <div>
+          <button onClick={this.handlePlusButton}>+1</button>
+          <button onClick={this.handleMinusButton}>-1</button>
+        </div>
+      </div>
     )
   }
 }
